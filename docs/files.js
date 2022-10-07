683020385c2906c7068b51819f3be60dfbd47e94
Muhammed Nget <nget7737@gmail.com>
2022-10-07 12:38:17 +0000

    Added two new .txt files

--- docs/files.js
+++ docs/files.js
@@ -1,3 +1,27 @@
 const fs = require('fs');
 
 // Reading files
+// fs.readFile('./blog1.txt', (err, data) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+
+
+// Writing to files
+// fs.writeFile('./blog1.txt', 'Hello Software Chronicles', () => {
+//     console.log('File has been successfully written');
+// })
+
+// Writing to an un-created file
+// fs.writeFile('./blog2.txt', 'Hi Brix Software Monsters', () => {
+//     console.log('File has been successfully written');
+// })
+
+// Appending to files
+// fs.appendFile('.blog2.txt', ' .I hope everybody is ready for the project', () => {
+//     console.log('File has been successfully appended with text');
+// })
+
+
